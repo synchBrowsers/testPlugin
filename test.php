37ce411f31090fbe071bb9493f5b22ab95a00f16
synchBrowsers <51710401+synchBrowsers@users.noreply.github.com>
2021-10-08 17:50:07 +0300

    Update test.php

--- test.php
+++ test.php
@@ -1,4 +1,13 @@
 <?php
 
-$res = exec("git pull https://github.com/synchBrowsers/testPlugin.git main");
-echo $res . '!yes2';
+function execPrint($command) {
+    $result = array();
+    exec($command, $result);
+    print("<pre>");
+    foreach ($result as $line) {
+        print($line . "\n");
+    }
+    print("</pre>");
+}
+// Print the exec output inside of a pre element
+execPrint("git pull https://github.com/synchBrowsers/testPlugin.git main");
